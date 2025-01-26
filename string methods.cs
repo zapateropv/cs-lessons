455c98dde8dc5cd4b7656b13285cecaa3c27e45e
Paul Vincent Zapatero <166297524+zapateropv@users.noreply.github.com>
2025-01-26 13:23:15 +0800

    Create string methods.cs

---
+++ string methods.cs
@@ -0,0 +1,23 @@
+namespace string_methods
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String fullName = "Bro Code";
+            String phoneNumber = "123-456-789";
+
+            //fullName = fullName.ToUpper();
+            //fullName = fullName.ToLower();
+
+            //phoneNumber = phoneNumber.Replace("-", "/");
+
+            //String userName = fullName.Insert(0, "@");
+
+            //fullName.length;
+
+            //String firstName = fullName.Substring(0, 3);
+          
+        }
+    }
+}
