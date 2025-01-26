5fce75b1afc757a0f86c2aae532d3639b79da851
Paul Vincent Zapatero <166297524+zapateropv@users.noreply.github.com>
2025-01-26 13:01:03 +0800

    Create math class.cs

---
+++ math class.cs
@@ -0,0 +1,23 @@
+namespace math_class
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x = 3;
+            double y = 5;
+
+            //double a = Math.Pow(x, 5);
+            //double b = Math.Sqrt(x);
+            //double c = Math.Abs(x);
+            //double d = Math.Round(x);
+            //double e = Math.Ceiling(x);
+            //double f = Math.Floor(x);
+            //double g = Math.Max(x, y);
+            //double h = Math.Min(x, y);
+
+
+            
+        }
+    }
+}
